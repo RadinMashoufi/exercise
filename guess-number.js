ddc96106775c20fee33ea27b8e4e0c82df3bdbc8
radin <radin.mashoufi@gmail.com>
2023-08-23 17:39:43 +0330

    fix-the-issues

--- guess-number.js
+++ guess-number.js
@@ -3,6 +3,8 @@ const guessInputEl = document.getElementById("guessInput")
 const guessCounterEl = document.getElementById("guessCounter")
 const guessButtonEl = document.getElementById("guessButton")
 const nextMissionEl = document.getElementById("nextMission")
+const staticHeadingEl = document.getElementById("staticHeading")
+const showResultEl = document.getElementById("showResult")
 let randomNumber = parseInt(Math.random() * 100)
 console.log(randomNumber)
 let count = 0;
@@ -19,10 +21,19 @@ guessButtonEl.addEventListener('click', () => {
         }
         else {
             guessResultEl.innerHTML = "عددی که حدس زده اید درست است"
+            guessButtonEl.style.display = 'none'
+            guessInputEl.style.display = 'none'
+            staticHeadingEl.style.display = 'none'
+            guessCounterEl.style.display = 'none'
+            nextMissionEl.style.display = 'inline'
         }
     if (count > 5) {
         guessCounterEl.innerHTML = "شما بیش از 5 بار حدس زدید و باختید"
+        showResultEl.innerHTML = `عدد انتخاب شده ${randomNumber} بود`
         nextMissionEl.style.display = 'none'
         guessResultEl.style.display = 'none'
+        guessInputEl.style.display = 'none'
+        staticHeadingEl.style.display = 'none'
+        guessButtonEl.style.display = 'none'
     }
 })
--- level-up.js
+++ level-up.js
@@ -2,6 +2,7 @@ const luckyNumberEl = document.getElementById("luckyNumber")
 const counterButtonEl = document.getElementById("counterButton")
 const finalNumberEl = document.getElementById("finalNumber")
 const gameStatusEl = document.getElementById("gameStatus")
+const nextMissionEl = document.getElementById("nextMission")
 let selectedNumber = parseInt(Math.random() * 100)
 luckyNumberEl.innerHTML = selectedNumber
 let count = 0;
@@ -12,5 +13,7 @@ counterButtonEl.addEventListener('click', () => {
         gameStatusEl.innerHTML = "عالی. شما برنده شدید"
         counterButtonEl.style.display = 'none'
         luckyNumberEl.style.display = 'none'
+        finalNumberEl.style.display = 'none'
+        nextMissionEl.style.display = 'inline'
     }
 })
